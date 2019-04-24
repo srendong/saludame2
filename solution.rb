ef936f3ddc3fa1a9da5cda02962b37627cc59907
Samuel Rendon G <s.redon.g@gmail.com>
2019-04-24 09:49:59 -0500

    saludame23

--- solution.rb
+++ solution.rb
@@ -1,7 +1,14 @@
+
+
 require 'sinatra'
 
 get '/' do
-  "<h1>hola mundo</h1>"
+if  params[:nombre].nil? || params[ :nombre].empty?
+  "<h1>Hola desconocido!</h1>" # si ingreso al local host sin un parametro imprime esto
+
+  else
+    "<h1>Hola #{params[:nombre]}!</h1>" #si ingreso al localhost con un parametro imprime esto
+end
 end
 
 get '/:nombre' do
