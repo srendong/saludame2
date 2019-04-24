28ec5a5139ea88cfdc0dab25d95c6889a6cda8da
Samuel Rendon G <s.redon.g@gmail.com>
2019-04-24 10:00:54 -0500

    saludame24

--- solution.rb
+++ solution.rb
@@ -1,17 +1,7 @@
-
-
 require 'sinatra'
 
-get '/' do
-if  params[:nombre].nil? || params[ :nombre].empty?
-  "<h1>Hola desconocido!</h1>" # si ingreso al local host sin un parametro imprime esto
-
-  else
-    "<h1>Hola #{params[:nombre]}!</h1>" #si ingreso al localhost con un parametro imprime esto
-end
-end
 
 get '/:nombre' do
 
-  "<h1 style=\"text-transform: capitalize\">Hola #{params[:nombre]}!</h1>"
+    @codigo = "Hola #{params[:nombre]}!</h1>"
 end
